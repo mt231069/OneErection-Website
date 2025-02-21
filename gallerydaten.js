0bef55b0d0c65e8121d245818fb90aa3843fc65a
mt231069 <161435517+mt231069@users.noreply.github.com>
2025-02-21 13:55:31 +0100

    Add files via upload

---
+++ gallerydaten.js
@@ -0,0 +1,53 @@
+const gallery = [
+  {
+    id: "2faf3c97-3eec-415c-b253-13c1a94f9f39",
+    description: "Planet Festival Tour Part 1, Szene Wien",
+    albumDescription: "Super Schöne Fotos vom Philip aus der Szene",
+    titlePicture: "gallerypics/album1/P9200136.JPG",
+    pictures: ["gallerypics/album1/P9200136.JPG", "gallerypics/album1/P9200138.JPG", "gallerypics/album1/P9200139.JPG", "gallerypics/album1/P9200140.JPG", "gallerypics/album1/P9200141.JPG", "gallerypics/album1/P9200146.JPG", "gallerypics/album1/P9200147.JPG", "gallerypics/album1/P9200148.JPG", "gallerypics/album1/P9200149.JPG", "gallerypics/album1/P9200150.JPG", "gallerypics/album1/P9200151.JPG", "gallerypics/album1/P9200153.JPG"],
+    videos: ["gallerypics/album1/P9200137.MOV", "gallerypics/album1/P9200155.MOV"]
+  },
+  {
+    id: "0e9d0a0a-d750-43f0-8709-535552845b0b",
+    description: "Planet Festival Tour Part 2, Szene Wien",
+    albumDescription: "Auch seehr feine Fotos vom Markus",
+    titlePicture: "gallerypics/album2/DSC_4381.jpg",
+    pictures: ["gallerypics/album2/DSC_4310.jpg", "gallerypics/album2/DSC_4311.jpg", "gallerypics/album2/DSC_4315.jpg", "gallerypics/album2/DSC_4318.jpg", "gallerypics/album2/DSC_4320.jpg", "gallerypics/album2/DSC_4322.jpg", "gallerypics/album2/DSC_4323.jpg", "gallerypics/album2/DSC_4325.jpg", "gallerypics/album2/DSC_4330.jpg", "gallerypics/album2/DSC_4334.jpg", "gallerypics/album2/DSC_4356.jpg", "gallerypics/album2/DSC_4386.jpg", "gallerypics/album2/DSC_4388.jpg", "gallerypics/album2/DSC_4405.jpg"]
+  },
+  {
+    id: "7241d6b1-032e-43f8-a5ed-11c4af545477",
+    description: "Album Release von Bitte Pssst im Replugged",
+    albumDescription: "ganz tolle Bilder vom Yuriy",
+    titlePicture: "gallerypics/album3/24_07_13_OneErection-101.jpg",
+    pictures: ["gallerypics/album3/24_07_13_OneErection-52.jpg", "gallerypics/album3/24_07_13_OneErection-159.jpg", "gallerypics/album3/24_07_13_OneErection-31.jpg", "gallerypics/album3/24_07_13_OneErection-33.jpg", "gallerypics/album3/24_07_13_OneErection-36.jpg", "gallerypics/album3/24_07_13_OneErection-47.jpg", "gallerypics/album3/24_07_13_OneErection-74.jpg", "gallerypics/album3/24_07_13_OneErection-76.jpg", "gallerypics/album3/24_07_13_OneErection-88.jpg", "gallerypics/album3/24_07_13_OneErection-101.jpg", "gallerypics/album3/24_07_13_OneErection-111.jpg", "gallerypics/album3/24_07_13_OneErection-115.jpg", "gallerypics/album3/24_07_13_OneErection-123.jpg", "gallerypics/album3/24_07_13_OneErection-133.jpg", "gallerypics/album3/24_07_13_OneErection-142.jpg", "gallerypics/album3/24_07_13_OneErection-153.jpg"]
+  },
+  {
+    id: "ba1a1324-23ab-4156-8c8f-31d615363c5e",
+    description: "Unser erster Gig im Pfarrkeller",
+    albumDescription: "Bilder vom besten Handy",
+    titlePicture: "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.52 (1).jpeg",
+    pictures: ["gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.45 (1).jpeg",
+    "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.45 (2).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.45 (3).jpeg",
+    "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.45 (4).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.45.jpeg","gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.46 (1).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.46.jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.47 (1).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.47 (2).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.47.jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.48 (1).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.48.jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.49 (1).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.49 (2).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.49 (3).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.49 (4).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.49.jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.51 (1).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.51 (2).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.51 (3).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.51.jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.52 (1).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.52 (2).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.52 (3).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.52 (4).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.52.jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.53 (1).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.53 (2).jpeg", "gallerypics/album6/WhatsApp Image 2024-10-03 at 18.39.53.jpeg"]
+  },
+  {
+    id: "5fd7b2db-b19f-4252-8453-cca6615da6d1",
+    description: "Maifest 2024 in der Pfarre",
+    albumDescription: "Am besten werden die Fotos, wenn man sie über WhatsApp verschickt!",
+    titlePicture: "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.52 (1).jpeg",
+    pictures: ["gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.48.jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.49 (1).jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.49.jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.50.jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.52 (1).jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.52 (2).jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.52.jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.55.jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.56 (1).jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.56.jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.57 (1).jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.57.jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.58 (1).jpeg",
+    "gallerypics/album5/WhatsApp Image 2024-10-03 at 18.53.58.jpeg"]
+  },
+];
