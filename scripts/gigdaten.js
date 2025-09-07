24b8ac1f7e37fb53902b93a950211617ad396227
mt231069 <161435517+mt231069@users.noreply.github.com>
2025-09-07 08:57:26 +0200

    Modify gigdaten event details and ticket status
    Updated event details and ticket information for 'PrüfungsIRRElevant II - Back to the Pit'.

--- scripts/gigdaten.js
+++ scripts/gigdaten.js
@@ -1,29 +1,17 @@
 const gigdaten = [
     {
-       preisCent: "tba",
-       picture2:"Produktpics/ticket_mockup.png",
-       name: "PrüfungsIRRElevant II - Back to the Pit",
-       location: "Replugged",
-       adresse: "Lerchenfelder Str. 23, 1070 Wien",
-       date: "2025-10-25",
-       entry: "19:30",
-       showtime: "19:30",
-       info: "Online Kartenverkauf kommt",
-       ticket: false,
-       whereToBuy: "/",
-       id: "c491d55d-69f7-48b2-8707-a0b109cf3a3e"
+      preisCent: "10",
+      picture2:"Produktpics/ticket_mockup.png",
+      name: "PrüfungsIRRElevant II - Back to the Pit",
+      location: "Replugged",
+      adresse: "Lerchenfelder Str. 23, 1070 Wien",
+      date: "2025-10-25",
+      entry: "19:30",
+      showtime: "19:30",
+      info: "Online Vorverkauf und Abendkassa (12€)",
+      ticket: true,
+      ticketLink: "https://www.tickettailor.com/events/heartbreakrebellion/1851215",
+      whereToBuy: "/",
+      id: "c491d55d-69f7-48b2-8707-a0b109cf3a3e"
      }
-  // {
-  //   preisCent: "20",
-  //   picture2:"Produktpics/ticket_mockup.png",
-  //   name: "Planet Festival Tour",
-  //   location: "Gasometer",
-  //   adresse: "Guglgasse 8, 1110 Wien",
-  //   date: "2025-05-17",
-  //   entry: "16:00",
-  //   showtime: "16:00",
-  //   info: " DM / Email für Tickets",
-  //   ticket: true,
-  //   id: "14535900-e771-435c-86a6-76ab6146f1bb"
-  // },
 ];
