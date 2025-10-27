7387f7e38bbd03eb0e4c5f21f88df73f17014845
mt231069 <161435517+mt231069@users.noreply.github.com>
2025-10-27 17:24:10 +0100

    Comment out event object in gigdaten array
    Commented out an event object in the gigdaten array.

--- scripts/gigdaten.js
+++ scripts/gigdaten.js
@@ -1,17 +1,17 @@
 const gigdaten = [
-    {
-      preisCent: "10",
-      picture2:"Produktpics/ticket_mockup.png",
-      name: "PrüfungsIRRElevant II - Back to the Pit",
-      location: "Replugged",
-      adresse: "Lerchenfelder Str. 23, 1070 Wien",
-      date: "2025-10-25",
-      entry: "19:00",
-      showtime: "19:30",
-      info: "Online Vorverkauf und Abendkassa (12€)",
-      ticket: true,
-      ticketLink: "https://www.tickettailor.com/events/heartbreakrebellion/1851215",
-      whereToBuy: "/",
-      id: "c491d55d-69f7-48b2-8707-a0b109cf3a3e"
-     }
+  // {
+  //   preisCent: "10",
+  //   picture2:"Produktpics/ticket_mockup.png",
+  //   name: "PrüfungsIRRElevant II - Back to the Pit",
+  //   location: "Replugged",
+  //   adresse: "Lerchenfelder Str. 23, 1070 Wien",
+  //   date: "2025-10-25",
+  //   entry: "19:00",
+  //   showtime: "19:30",
+  //   info: "Online Vorverkauf und Abendkassa (12€)",
+  //   ticket: true,
+  //   ticketLink: "https://www.tickettailor.com/events/heartbreakrebellion/1851215",
+  //   whereToBuy: "/",
+  //   id: "c491d55d-69f7-48b2-8707-a0b109cf3a3e"
+  //  }
 ];
