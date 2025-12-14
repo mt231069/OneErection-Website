6a333b94ecb691c550b56b24b07c73ead6fa79bb
mt231069 <161435517+mt231069@users.noreply.github.com>
2025-12-14 13:29:03 +0100

    Update event details in gigdaten.js

--- scripts/gigdaten.js
+++ scripts/gigdaten.js
@@ -1,17 +1,17 @@
 const gigdaten = [
-  <!-- {
+ {
      preisCent: "0",
      picture2:"Produktpics/ticket_mockup.png",
-     name: "Openstage in der TU Wien",
-     location: "Openstage in der TU Wien",
-     adresse: "Prechtlsaal",
-     date: "2025-11-27",
-     entry: " 16:00",
+     name: "Playoff PP Tour",
+     location: "Szene Wien",
+     adresse: "Hauffgasse 26, 1110 Wien",
+     date: "2026-01-30",
+     entry: " 18:30",
      showtime: " /",
-     info: "gratis Eintritt",
+     info: "DM for Tickets",
      ticket: false,
      ticketLink: "https://www.tickettailor.com/events/heartbreakrebellion/1851215",
      whereToBuy: "/",
      id: "c491d55d-69f7-48b2-8707-a0b109cf3a3e"
-    }  -->
+    } 
 ];
