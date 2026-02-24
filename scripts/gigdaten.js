45826ffb958a0c22315024ff3dadd730067c4d69
mt231069 <161435517+mt231069@users.noreply.github.com>
2026-02-24 15:17:40 +0100

    Update gigdaten with new event details

--- scripts/gigdaten.js
+++ scripts/gigdaten.js
@@ -1,19 +1,17 @@
 const gigdaten = [
-/* 
 {
-     preisCent: "0",
+     preisCent: "7",
      picture2:"Produktpics/ticket_mockup.png",
-     name: "Playoff PP Tour",
-     location: "Szene Wien (Playoff PP Tour)",
-     adresse: "Hauffgasse 26, 1110 Wien",
-     date: "2026-01-30",
-     entry: " 18:30",
-     showtime: " /",
-     info: "DM for Tickets",
+     name: "Smörebröht Metak, One Erection",
+     location: "Kramladen)",
+     adresse: "39/40 u-bahnbogen, 1080",
+     date: "2026-03-27",
+     entry: " 19:30",
+     showtime: "20:00",
+     info: "Ticketlink kommt noch, Abendkasse 10€",
      ticket: false,
      ticketLink: "https://www.tickettailor.com/events/heartbreakrebellion/1851215",
      whereToBuy: "/",
      id: "c491d55d-69f7-48b2-8707-a0b109cf3a3e"
     }
-    */
 ];
