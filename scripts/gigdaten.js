f1f16ad9ffa5526792d8dda79bfe93caa83c0bd6
mt231069 <161435517+mt231069@users.noreply.github.com>
2026-03-11 22:47:23 +0100

    Update gigdaten.js

--- scripts/gigdaten.js
+++ scripts/gigdaten.js
@@ -2,15 +2,15 @@ const gigdaten = [
 {
      preisCent: "7 ",
      picture2:"Produktpics/ticket_mockup.png",
-     name: "Smörebröht Metak, One Erection",
-     location: "Kramladen mit Smörebröht und Metak",
+     name: "Smörebröht, Metak, One Erection",
+     location: "Kramladen",
      adresse: "39/40 u-bahnbogen, 1080",
      date: "2026-03-27",
      entry: " 19:30",
      showtime: " 20:00",
-     info: "Ticketlink kommt noch, Abendkasse 10€",
-     ticket: false,
-     ticketLink: "https://www.tickettailor.com/events/heartbreakrebellion/1851215",
+     info: "Abendkasse 10€",
+     ticket: true,
+     ticketLink: "https://one-erection.sumupstore.com/produkt/tickets-kramladen-27-03-2026",
      whereToBuy: "/",
      id: "c491d55d-69f7-48b2-8707-a0b109cf3a3e"
     }
